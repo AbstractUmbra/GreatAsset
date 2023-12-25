d5324e33ca65098c64078e35ac14c1136ba9ea96
Alex Nørgaard <umbra@abstractumbra.dev>
2023-12-25 22:07:08 +0000

    fix tests

--- tests/test_save_file.py
+++ tests/test_save_file.py
@@ -5,57 +5,58 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Literal
 
 from great_asset import SaveFile, ShipUnlocks
 
-SAVE_FILE_1 = Path("./save_files/LCSaveFile1")
-SAVE_FILE_2 = Path("./save_files/LCSaveFile2")
+BASE_PATH = Path(__file__).parent
+
+
+def make_save(number: Literal[1, 2, 3]) -> SaveFile:
+    path = BASE_PATH / f"./save_files/LCSaveFile{number}"
+    return SaveFile(path)
 
 
 class TestSaveFile:
-    def __init__(self) -> None:
-        self.save_file_1 = SaveFile(SAVE_FILE_1)
-        self.save_file_2 = SaveFile(SAVE_FILE_2)
-
-    def _reset(self) -> None:
-        """Reset the files between each test so we don't lose 'default' data."""
-        self.save_file_1 = SaveFile(SAVE_FILE_1)
-        self.save_file_2 = SaveFile(SAVE_FILE_2)
-
     def test_credit_changes(self) -> None:
         # check default values
-        assert self.save_file_1.credits == 1605
-        assert self.save_file_2.credits == 585
 
-        self.save_file_1.credits = 2820
-        self.save_file_2.credits = 312
+        save_file_1 = make_save(1)
+        save_file_2 = make_save(2)
+        assert save_file_1.credits == 1605
+        assert save_file_2.credits == 585
 
-        self.save_file_1.serialise(write=False)
-        assert self.save_file_1._inner_data["GroupCredits"]["value"] == 2820
+        save_file_1.credits = 2820
+        save_file_2.credits = 312
 
-        self.save_file_2.serialise(write=False)
-        assert self.save_file_2._inner_data["GroupCredits"]["value"] == 312
+        save_file_1.serialise(write=False)
+        assert save_file_1._inner_data["GroupCredits"]["value"] == 2820
 
-        self._reset()
+        save_file_2.serialise(write=False)
+        assert save_file_2._inner_data["GroupCredits"]["value"] == 312
 
     def test_adding_teleporter(self) -> None:
-        self.save_file_1.unlock_ship_upgrades(ShipUnlocks.teleporter)
+        save_file_1 = make_save(1)
 
-        self.save_file_1.serialise(write=False)
+        save_file_1.unlock_ship_upgrades(ShipUnlocks.teleporter)
+
+        save_file_1.serialise(write=False)
 
         assert (
-            ShipUnlocks.teleporter.serialised_value in self.save_file_1._inner_data["UnlockedShipObjects"]["value"]
-            and self.save_file_1._inner_data["ShipUnlockStored_Teleporter"]["value"] is True
+            ShipUnlocks.teleporter.serialised_value in save_file_1._inner_data["UnlockedShipObjects"]["value"]
+            and save_file_1._inner_data["ShipUnlockStored_Teleporter"]["value"] is True
         )
 
     def test_removing_teleporter(self) -> None:
-        self.save_file_2.unlock_ship_upgrades(ShipUnlocks.teleporter)
+        save_file_2 = make_save(2)
 
-        self.save_file_2.remove_ship_upgrades(ShipUnlocks.teleporter)
+        save_file_2.unlock_ship_upgrades(ShipUnlocks.teleporter)
 
-        self.save_file_2.serialise(write=False)
+        save_file_2.remove_ship_upgrades(ShipUnlocks.teleporter)
+
+        save_file_2.serialise(write=False)
 
         assert (
-            ShipUnlocks.teleporter.serialised_value not in self.save_file_2._inner_data["UnlockedShipObjects"]["value"]
-            and self.save_file_2._inner_data["ShipUnlockStored_Teleporter"]["value"] is False
+            ShipUnlocks.teleporter.serialised_value not in save_file_2._inner_data["UnlockedShipObjects"]["value"]
+            and save_file_2._inner_data["ShipUnlockStored_Teleporter"]["value"] is False
         )
